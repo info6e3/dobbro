1271d3e4353a54184763740c54fa276b7c410599
info6e3 <info6e3@gmail.com>
2023-06-20 05:02:38 +0300

    fix navigate on click and key

--- src/components/Poems_Links.js
+++ src/components/Poems_Links.js
@@ -3,24 +3,27 @@ import Image_ArrowBack from "../images/interface/ArrowBack_active.png";
 import Image_ArrowForward from "../images/interface/ArrowForward_active.png";
 import Image_BlackArrowBack from "../images/interface/BlackArrowBack.png";
 import Image_BlackArrowForward from "../images/interface/BlackArrowForward_active.png";
-
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 import Image_NiznebaLink from "../images/links/nizneba.png";
 import Image_PrizhokLink from "../images/links/prizhok.png";
 
 function Poems_Links({next, prev, theme}) {
+    const navigate = useNavigate();
+
     window.onkeydown = function (e) {
         if (e.keyCode === 37) {
             if (prev) {
                 document.cookie = "poem_number=0";
-                window.location.href = `${process.env.REACT_APP_URL}${prev.ref}`;
+                navigate(prev.ref)
+                window.scrollTo(0, 0)
             }
         }
         if (e.keyCode === 39) {
             if (next) {
                 document.cookie = "poem_number=0";
-                window.location.href = `${process.env.REACT_APP_URL}${next.ref}`;
+                navigate(next.ref)
+                window.scrollTo(0, 0)
             }
         }
     };
@@ -42,13 +45,13 @@ function Poems_Links({next, prev, theme}) {
     return (
         <div className={"PoemsLinksContainer unselectable"}>
             {
-                prev ? <Link to={prev.ref} className={"PoemsBackLink"}>
+                prev ? <Link  onClick={() => window.scrollTo(0, 0)}  to={prev.ref+'#0'} className={"PoemsBackLink"}>
                     <div id={"PoemsBackLink_Title"}>{linkTitle(prev)}</div>
                     <img id={"Image_ArrowBack"} src={theme === "white" ? Image_BlackArrowBack : Image_ArrowBack}/>
                 </Link> : null
             }
             {
-                next ? <Link to={next.ref} className={"PoemsForwardLink"}>
+                next ? <Link onClick={() => window.scrollTo(0, 0)} to={next.ref+'#0'} className={"PoemsForwardLink"}>
                     <div id={"PoemsForwardLink_Title"}>{linkTitle(next)}</div>
                     <img id={"Image_ArrowForward"} src={theme === "white" ? Image_BlackArrowForward : Image_ArrowForward}/>
                 </Link> : null
